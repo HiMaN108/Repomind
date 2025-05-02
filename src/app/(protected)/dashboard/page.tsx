a169061dece5b42134bc33257b5366c63027e429
HiMaN108 <him2000.maurya@gmail.com>
2025-05-02 12:09:56 +0530

    remove dynamic ssr

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -9,8 +9,10 @@ import CommitLog from './commit-log'
 import AskQUestionCard from './ask-question-card'
 import MeetingCard from './meeting-card'
 import ArchiveButton from './archive-button'
-import InviteButton from './invite-button'
+// import InviteButton from './invite-button'
+const InviteButton = dynamic(() => import('./invite-button'), { ssr: false})
 import TeamMembes from './team-member'
+import dynamic from 'next/dynamic'
 
 const DashboardPage = () => {
     const { project } = useProject()
